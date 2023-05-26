e5e4356ca5bcd12ab605c0d4e704310ee5595a07
shablonov <shablonov@it-limon.ru>
2023-05-26 12:52:01 +0500

    automate docker deployment

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,21 @@
 FROM node:16.14.2-slim
 
+#Define the working directory
 WORKDIR /usr/src/app
 
-# Uncomment to skip the chromium download when installing puppeteer. If you do,
-# you'll need to launch puppeteer with:
-#     browser.launch({executablePath: 'google-chrome-stable'})
+#Skip the chromium download for puppeteer
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+#Supply username and password at build-time using --build-arg
 ARG username
 ARG passwd
 
+#For tidiness, define the user's home dir
+ENV USR_HOME=/home/${username}
+
+#Get openssh and sudo
 RUN apt-get update && apt-get install -y openssh-server sudo
 
-# Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
-# Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
-# installs, work.
+#Ensure that the bundled chromium is working with puppeteer
 RUN apt-get update && apt-get install gnupg wget -y && \
   wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
   sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
@@ -21,17 +23,23 @@ RUN apt-get update && apt-get install gnupg wget -y && \
   apt-get install google-chrome-stable -y --no-install-recommends && \
   rm -rf /var/lib/apt/lists/*
 
+#Make the new user using the args supplied
 RUN useradd $username
 RUN echo "$username:$passwd" | chpasswd
 RUN usermod -aG sudo $username
 
+#Set up sshd keys
 RUN mkdir /var/run/sshd
 COPY assets/docker/sshd_config /etc/ssh/
-COPY assets/docker/init.sh /usr/local/bin/
+#Generate your own keys using grunt before building the image
+COPY build/assets/ssh/id_rsa.pub ${USR_HOME}/.ssh/authorized_keys
+RUN chmod -R go= ${USR_HOME}/.ssh
+RUN chown -R ${username}:${username} ${USR_HOME}/.ssh
 
-# RUN service ssh start
-EXPOSE 2222 3000
+#Expose the ssh port
+EXPOSE 22
 
+#Copy over the site source
 COPY package*.json ./
 COPY server.mjs ./
 COPY index.js ./
@@ -42,6 +50,11 @@ COPY assets/onepage/index.js ./assets/onepage/index.js
 COPY assets/onepage/resume.hbs ./assets/onepage/resume.hbs
 COPY assets/onepage/style.css ./assets/onepage/style.css
 
+#Copy over the initialization script
+COPY assets/docker/init.sh /usr/local/bin/
+
+#Prepeare the node environment
 RUN npm ci --omit=dev
 
+#Initialize the container using the init script
 CMD ["bash","init.sh"]
--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,51 @@ module.exports = function(grunt) {
             run_server: {
                 cmd: "node server.mjs"
             },
+            generate_keys: {
+                cmd: function(passphrase) {
+                    const fs = require('fs');
+                    const path = require("path");
+
+                    grunt.log.writeln("Generating SSH keys...")
+                    process.env['SSH_ASKPASS'] = passphrase
+                    process.env['DISPLAY'] = 1
+                    const sshPath = __dirname + "\\build\\assets\\ssh"
+
+                    //If the ssh folder doesn't exist, make one
+                    if (!fs.existsSync(sshPath)){
+                        fs.mkdirSync(sshPath, { recursive: true });
+                    }
+                    //If it exists but isn't empty, delete everything in it
+                    else if (fs.existsSync(sshPath + "\\id_rsa")) {
+                        grunt.log.writeln("Previous SSH keys found. Overwriting...")
+                        fs.readdir(sshPath, (err, files) => {
+                            if (err) throw err;
+
+                            for (const file of files) {
+                              fs.unlink(path.join(sshPath, file), (err) => {
+                                if (err) throw err;
+                              });
+                            }
+                        })
+                    }
+                    return `ssh-keygen -t rsa -b 4096 -f ${sshPath}\\id_rsa`
+                }
+            },
+            build_img: {
+                cmd: function(username, pwd, img_name="resume_server") {
+                    grunt.log.writeln("Building Docker image...")
+                    return `docker build -t ${img_name} --build-arg username=${username} --build-arg passwd=${pwd} .`
+                }
+            },
+            run_docker: {
+                cmd: function(img_name="resume_server") {
+                    const dotenv = require('dotenv')
+                    dotenv.config();
+                    const port = process.env.PORT;
+                    
+                    return `docker run -p ${port}:${port} -p 22:22 --env-file ./.env ${img_name}`
+                }
+            }
         },
         copy: {
             resumejson: {
@@ -104,4 +149,21 @@ module.exports = function(grunt) {
         'exec:run_server'
     ]);
     grunt.registerTask('compile:pug', ['exec:compile_pug']);
+    grunt.registerTask('deploy', 'Deploy the project into Docker', function(passphrase, username, pwd, img_name="resume_server") {
+        if (!passphrase) {
+            grunt.log.error("Passphrase is required to generate SSH keys.")
+            return false
+        }
+        if (!username) {
+            grunt.log.error("Username is required to build the Docker image.")
+            return false
+        }
+        if (!pwd) {
+            grunt.log.error("Password is required to build the Docker image.")
+            return false
+        }
+        grunt.log.writeln("Building project...")
+        grunt.task.run(['build', `exec:generate_keys:${passphrase}`, `exec:build_img:${username}:${pwd}:${img_name}`])
+    });
+    grunt.registerTask('run_docker', ['exec:run_docker']);
 }
